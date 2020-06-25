894fdc7438b00c76056e5acc202c451c858d9b8e
Aliasgar Mala <aliasgar.mala@gmail.com>
2020-06-25 09:28:15 -0400

    Initial Commit

---
+++ BillEngine/BillEngine.h
@@ -0,0 +1,19 @@
+//
+//  BillEngine.h
+//  BillEngine
+//
+//  Created by Aliasgar Mala on 2020-06-25.
+//  Copyright © 2020 Aliasgar Mala. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BillEngine.
+FOUNDATION_EXPORT double BillEngineVersionNumber;
+
+//! Project version string for BillEngine.
+FOUNDATION_EXPORT const unsigned char BillEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BillEngine/PublicHeader.h>
+
+
